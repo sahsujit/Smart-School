d80b54f32769ae451ba9775c453f3880b84bca7e
sahsujit <sujit8877123sah@gmail.com>
2026-02-20 15:39:15 +0545

    complited

--- smart-school-backend/routes/assignmentRoutes.js
+++ smart-school-backend/routes/assignmentRoutes.js
@@ -1,39 +1,5 @@
 
 
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   getAllAssignments,
-//   createAssignment,
-//   getTeacherAssignments,
-// } = require("../controllers/assignmentController");
-
-// const { authMiddleware, roleMiddleware } = require("../middleware/authMiddleware");
-
-// // Everyone logged in can fetch assignments
-// router.get("/public", getAllAssignments);
-
-// // Teacher-only dashboard
-// router.get("/teacher", authMiddleware, roleMiddleware("teacher"), getTeacherAssignments);
-
-// // Teacher-only: create assignments
-// router.post("/", authMiddleware, roleMiddleware("teacher"), createAssignment);
-
-// module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const {
